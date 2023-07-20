677c22e743a4eb9a5cc9abfc206adf4bd36dd2b1
andre83411 <andreamontes.rei@gmail.com>
2023-07-19 20:06:33 -0600

    backend orde.js syntax error

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -1,6 +1,8 @@
 "use strict";
-const stripe = require("stripe")("sk_test_51NLz3TJFbnWEjz9NzvE8PVFYMsIxpBc90JcLxe6LXhBn81YE0rsrRjfGyxzHtZjbdFVAWR6afgPe6gKN5IKSukBV00oyiDCY1Y");
-
+import Stripe from 'stripe';
+const stripe = new Stripe("sk_test_51NLz3TJFbnWEjz9NzvE8PVFYMsIxpBc90JcLxe6LXhBn81YE0rsrRjfGyxzHtZjbdFVAWR6afgPe6gKN5IKSukBV00oyiDCY1Y", {
+  apiVersion: '2022-11-15',
+})
 /**
  *  order controller
  */
