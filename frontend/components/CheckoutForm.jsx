add14f0cd9db37027cd8071200edb513dcb640c2
andre83411 <andreamontes.rei@gmail.com>
2023-07-22 11:50:55 -0600

    trying another conditional for createOrder

--- frontend/components/CheckoutForm.jsx
+++ frontend/components/CheckoutForm.jsx
@@ -134,7 +134,7 @@ export default function CheckoutForm() {
 
       console.log("Response data:", response); 
 
-      if (response.data.createOrder) {
+      if (response.createOrder.data.attributes.token) {
         alert("Transaction Successful, continue your shopping");
         setData(INITIAL_STATE);
         resetCart();
