ca020940bf7eea60217707e8a834ddbca47f514a
andre83411 <andreamontes.rei@gmail.com>
2024-01-15 13:27:15 -0700

    added gitignore file

--- frontend/utils/centsToDollars.js
+++
@@ -1,5 +0,0 @@
-export function centsToDollars(cents) {
-    const dollars = Math.floor(cents / 100);
-    const centsRemainder = cents % 100;
-    return dollars + "." + centsRemainder.toString().padStart(2, "0");
-  }
